c01dc2b6b4e543c6b2f2bd9d6795dd6f3cf66355
Wisen Tanasa <wisen@ceilfors.com>
2019-03-05 06:07:02 +0000

    Delete unused files

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,9 +39,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     { doc: "introduction/getting-started", label: "Docs" },
-    { doc: "doc4", label: "API" },
-    { page: "help", label: "Help" },
-    { blog: true, label: "Blog" }
+    { page: "help", label: "Help" }
   ],
 
   // If you have users set above, you add it here:
